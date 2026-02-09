f721a21a7de1c596d0dc744410ad64b43648a291
enol <enol.arregui07@somo.eus>
2026-02-09 09:46:07 +0100

    Proyecto CalculadoraGIT01 clase Calculadora control de la division entre 0

--- src/calculadora01/Calculadora01.java
+++ src/calculadora01/Calculadora01.java
@@ -6,13 +6,19 @@ public class Calculadora01 {
 		return a + b;
 	}
 	
-	public int restar(int a, int b) { return a - b;
+	public int restar(int a, int b) { 
+		return a - b;
 	}
 
-	public int multiplicar(int a, int b) { return a * b;
+	public int multiplicar(int a, int b) { 
+		return a * b;
 	}
 
-	public double dividir(int a, int b) { return (double) a / b;
-	}
+	public double dividir(int a, int b) { if (b == 0) {
+		throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
+		return (double) a / b;
+		}
+
 
 }
